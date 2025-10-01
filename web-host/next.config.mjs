f80b7a2effb244f747c8e1e4e0c645e00bee3427
Mason Bickers <mason@bickers.co.uk>
2025-10-01 09:29:40 +0100

    Next wrapper to host Expo web build

---
+++ web-host/next.config.mjs
@@ -0,0 +1,7 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [{ source: '/:path*', destination: '/index.html' }];
+  },
+};
+export default nextConfig;
